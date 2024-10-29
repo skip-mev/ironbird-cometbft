cometbft/cometbft

Add GetSenders to mempool interface and Senders to Entry interface

internal/consensus/replay_stubs.go
@@ -39,14 +39,15 @@ func (emptyMempool) Update(
 ) error {
 	return nil
 }
-func (emptyMempool) Flush()                        {}
-func (emptyMempool) FlushAppConn() error           { return nil }
-func (emptyMempool) Contains(types.TxKey) bool     { return false }
-func (emptyMempool) TxsAvailable() <-chan struct{} { return make(chan struct{}) }
-func (emptyMempool) EnableTxsAvailable()           {}
-func (emptyMempool) TxsBytes() int64               { return 0 }
-func (emptyMempool) TxsFront() *clist.CElement     { return nil }
-func (emptyMempool) TxsWaitChan() <-chan struct{}  { return nil }
+func (emptyMempool) Flush()                                   {}
+func (emptyMempool) FlushAppConn() error                      { return nil }
+func (emptyMempool) Contains(types.TxKey) bool                { return false }
+func (emptyMempool) TxsAvailable() <-chan struct{}            { return make(chan struct{}) }
+func (emptyMempool) EnableTxsAvailable()                      {}
+func (emptyMempool) TxsBytes() int64                          { return 0 }
+func (emptyMempool) TxsFront() *clist.CElement                { return nil }
+func (emptyMempool) TxsWaitChan() <-chan struct{}             { return nil }
+func (emptyMempool) GetSenders(types.TxKey) ([]p2p.ID, error) { return nil, nil }
 
 // -----------------------------------------------------------------------------
 // newMockProxyApp uses ABCIResponses to give the right results.

mempool/clist_mempool.go
@@ -157,6 +157,18 @@ func NewCListMempool(
 	return mp
 }
 
+func (mem *CListMempool) GetSenders(txKey types.TxKey) ([]p2p.ID, error) {
+	mem.txsMtx.RLock()
+	defer mem.txsMtx.RUnlock()
+
+	elem, ok := mem.txsMap[txKey]
+	if !ok {
+		return nil, ErrTxNotFound
+	}
+	memTx := elem.Value.(*mempoolTx)
+	return memTx.Senders(), nil
+}
+
 func (mem *CListMempool) addToCache(tx types.Tx) bool {
 	return mem.cache.Push(tx)
 }

mempool/mempool.go
@@ -104,6 +104,9 @@ type Mempool interface {
 
 	// SizeBytes returns the total size of all txs in the mempool.
 	SizeBytes() int64
+
+	// GetSenders returns the list of node IDs from which we receive the given transaction.
+	GetSenders(txKey types.TxKey) ([]p2p.ID, error)
 }
 
 // PreCheckFunc is an optional filter executed before CheckTx and rejects
@@ -166,6 +169,9 @@ type Entry interface {
 
 	// IsSender returns whether we received the transaction from the given peer ID.
 	IsSender(peerID p2p.ID) bool
+
+	// Senders returns the list of registered peers that sent us the transaction.
+	Senders() []p2p.ID
 }
 
 // An iterator is used to iterate through the mempool entries.

mempool/mempoolTx.go
@@ -50,3 +50,12 @@ func (memTx *mempoolTx) addSender(peerID p2p.ID) bool {
 	}
 	return false
 }
+
+func (memTx *mempoolTx) Senders() []p2p.ID {
+	senders := make([]p2p.ID, 0)
+	memTx.senders.Range(func(key, _ any) bool {
+		senders = append(senders, key.(p2p.ID))
+		return true
+	})
+	return senders
+}

mempool/mocks/mempool.go
@@ -96,6 +96,36 @@ func (_m *Mempool) FlushAppConn() error {
 	return r0
 }
 
+// GetSenders provides a mock function with given fields: txKey
+func (_m *Mempool) GetSenders(txKey types.TxKey) ([]p2p.ID, error) {
+	ret := _m.Called(txKey)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetSenders")
+	}
+
+	var r0 []p2p.ID
+	var r1 error
+	if rf, ok := ret.Get(0).(func(types.TxKey) ([]p2p.ID, error)); ok {
+		return rf(txKey)
+	}
+	if rf, ok := ret.Get(0).(func(types.TxKey) []p2p.ID); ok {
+		r0 = rf(txKey)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]p2p.ID)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(types.TxKey) error); ok {
+		r1 = rf(txKey)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetTxByHash provides a mock function with given fields: hash
 func (_m *Mempool) GetTxByHash(hash []byte) types.Tx {
 	ret := _m.Called(hash)

mempool/nop_mempool.go
@@ -80,6 +80,9 @@ func (*NopMempool) Size() int { return 0 }
 // SizeBytes always returns 0.
 func (*NopMempool) SizeBytes() int64 { return 0 }
 
+// GetSenders always returns nil.
+func (*NopMempool) GetSenders(_ types.TxKey) ([]p2p.ID, error) { return nil, nil }
+
 // NopMempoolReactor is a mempool reactor that does nothing.
 type NopMempoolReactor struct {
 	service.BaseService

skip-mev/ironbird-cometbft

internal/consensus/replay_stubs.go
@@ -39,14 +39,15 @@ func (emptyMempool) Update(
 ) error {
 	return nil
 }
-func (emptyMempool) Flush()                        {}
-func (emptyMempool) FlushAppConn() error           { return nil }
-func (emptyMempool) Contains(types.TxKey) bool     { return false }
-func (emptyMempool) TxsAvailable() <-chan struct{} { return make(chan struct{}) }
-func (emptyMempool) EnableTxsAvailable()           {}
-func (emptyMempool) TxsBytes() int64               { return 0 }
-func (emptyMempool) TxsFront() *clist.CElement     { return nil }
-func (emptyMempool) TxsWaitChan() <-chan struct{}  { return nil }
+func (emptyMempool) Flush()                                       {}
+func (emptyMempool) FlushAppConn() error                          { return nil }
+func (emptyMempool) Contains(types.TxKey) bool                    { return false }
+func (emptyMempool) TxsAvailable() <-chan struct{}                { return make(chan struct{}) }
+func (emptyMempool) EnableTxsAvailable()                          {}
+func (emptyMempool) TxsBytes() int64                              { return 0 }
+func (emptyMempool) TxsFront() *clist.CElement                    { return nil }
+func (emptyMempool) TxsWaitChan() <-chan struct{}                 { return nil }
+func (emptyMempool) GetSenders(types.TxKey) ([]nodekey.ID, error) { return nil, nil }
 
 // -----------------------------------------------------------------------------
 // newMockProxyApp uses ABCIResponses to give the right results.

mempool/clist_mempool.go
@@ -157,6 +157,18 @@ func NewCListMempool(
 	return mp
 }
 
+func (mem *CListMempool) GetSenders(txKey types.TxKey) ([]nodekey.ID, error) {
+	mem.txsMtx.RLock()
+	defer mem.txsMtx.RUnlock()
+
+	elem, ok := mem.txsMap[txKey]
+	if !ok {
+		return nil, ErrTxNotFound
+	}
+	memTx := elem.Value.(*mempoolTx)
+	return memTx.Senders(), nil
+}
+
 func (mem *CListMempool) addToCache(tx types.Tx) bool {
 	return mem.cache.Push(tx)
 }

mempool/mempool.go
@@ -104,6 +104,9 @@ type Mempool interface {
 
 	// SizeBytes returns the total size of all txs in the mempool.
 	SizeBytes() int64
+
+	// GetSenders returns the list of node IDs from which we receive the given transaction.
+	GetSenders(txKey types.TxKey) ([]nodekey.ID, error)
 }
 
 // PreCheckFunc is an optional filter executed before CheckTx and rejects
@@ -166,6 +169,9 @@ type Entry interface {
 
 	// IsSender returns whether we received the transaction from the given peer ID.
 	IsSender(peerID nodekey.ID) bool
+
+	// Senders returns the list of registered peers that sent us the transaction.
+	Senders() []nodekey.ID
 }
 
 // An iterator is used to iterate through the mempool entries.

mempool/mempoolTx.go
@@ -50,3 +50,12 @@ func (memTx *mempoolTx) addSender(peerID nodekey.ID) bool {
 	}
 	return false
 }
+
+func (memTx *mempoolTx) Senders() []nodekey.ID {
+	senders := make([]nodekey.ID, 0)
+	memTx.senders.Range(func(key, _ any) bool {
+		senders = append(senders, key.(nodekey.ID))
+		return true
+	})
+	return senders
+}

mempool/mocks/mempool.go
@@ -96,6 +96,36 @@ func (_m *Mempool) FlushAppConn() error {
 	return r0
 }
 
+// GetSenders provides a mock function with given fields: txKey
+func (_m *Mempool) GetSenders(txKey types.TxKey) ([]nodekey.ID, error) {
+	ret := _m.Called(txKey)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetSenders")
+	}
+
+	var r0 []nodekey.ID
+	var r1 error
+	if rf, ok := ret.Get(0).(func(types.TxKey) ([]nodekey.ID, error)); ok {
+		return rf(txKey)
+	}
+	if rf, ok := ret.Get(0).(func(types.TxKey) []nodekey.ID); ok {
+		r0 = rf(txKey)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]nodekey.ID)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(types.TxKey) error); ok {
+		r1 = rf(txKey)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetTxByHash provides a mock function with given fields: hash
 func (_m *Mempool) GetTxByHash(hash []byte) types.Tx {
 	ret := _m.Called(hash)

mempool/nop_mempool.go
@@ -81,6 +81,9 @@ func (*NopMempool) Size() int { return 0 }
 // SizeBytes always returns 0.
 func (*NopMempool) SizeBytes() int64 { return 0 }
 
+// GetSenders always returns nil.
+func (*NopMempool) GetSenders(_ types.TxKey) ([]nodekey.ID, error) { return nil, nil }
+
 // NopMempoolReactor is a mempool reactor that does nothing.
 type NopMempoolReactor struct {
 	service.BaseService